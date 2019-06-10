9f37591f74fc4117af00241b3d32653d78b54114
AMeloSBC <alexandremelo52@gmail.com>
2019-06-10 20:10:42 -0300

    projeto teste algaworks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PainelNegociacaoComponent } from './painel-negociacao/painel-negociacao.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelNegociacaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
